df31edc13313683fdfc17274d241955d1c710daf
KrishCodesw <krishjain.w@gmail.com>
2026-03-31 21:11:28 +0530

    2026-03-31 21:11:28

--- javacollectionframeworks/arraylist/ALExttra.java
+++ javacollectionframeworks/arraylist/ALExttra.java
@@ -17,3 +17,20 @@ class StringLengthComparator implements Comparator<String> {
         return o1.length() - o2.length();
     }
 }
+
+class MyComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1 - o2;
+
+        // The integer that this compare method is returning is -
+        // Ascending order in o1,o2
+        // 1. negative ---- o1 should come before and o2 should come after 3-5=-2
+        // 2. positive ---- o2 should come after o1 and o1 should come before 5-3=2
+
+        // let's take o1=5, o2=3 and we want them in descending order ; that means as it
+        // is ; o1 should come before o2 ; result negative
+        // --- > o2-o1 will be the condition for descending
+
+    }
+}
