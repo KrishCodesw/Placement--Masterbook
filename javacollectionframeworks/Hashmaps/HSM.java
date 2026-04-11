17951214b27f4fa43b60d910500c4c16f926cd37
KrishCodesw <krishjain.w@gmail.com>
2026-04-11 16:23:28 +0530

    2026-04-11 16:23:28

--- javacollectionframeworks/Hashmaps/HSM.java
+++ javacollectionframeworks/Hashmaps/HSM.java
@@ -6,6 +6,7 @@ public class HSM {
     public static void main(String[] args) {
         HashMap<Integer, String> hmap = new HashMap<>();
         hmap.put(1, "Student1");
+        
         hmap.put(2, "Student2");
         hmap.put(3, "Student3");
         System.out.println(hmap);
@@ -14,7 +15,7 @@ public class HSM {
         // if no roll no then null returned
         System.out.println(hmap.containsKey(2));
         System.out.println(hmap.containsValue("Student1"));
-
+ 
         for (int i : hmap.keySet()) {
             System.out.println(hmap.get(i));
         }
