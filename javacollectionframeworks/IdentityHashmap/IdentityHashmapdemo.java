3cea8cd260750c8a4a2ed4f740f6b92e1e907c32
KrishCodesw <krishjain.w@gmail.com>
2026-04-12 13:37:27 +0530

    2026-04-12 13:37:27

--- javacollectionframeworks/IdentityHashmap/IdentityHashmapdemo.java
+++ javacollectionframeworks/IdentityHashmap/IdentityHashmapdemo.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.IdentityHashMap;
 
 public class IdentityHashmapdemo {
     public static void main(String[] args) {
@@ -11,7 +12,17 @@ public class IdentityHashmapdemo {
         // Equal nahi hai toh collision aayega and agar equal hai toh replace hojeaga
         // Equal will be checked with equals() method se
         System.out.println(Key1.equals(Key2));
-        
+
         System.out.println(map);
+
+        IdentityHashMap<String, Integer> map1 = new IdentityHashMap<>();
+        map.put(Key1, 1); // key,1
+        map.put(Key2, 2); // key,2
+
+        // The Identity Hashmap uses the hashCode function of the Object class itself
+        // and not of the String class which means the hashcode depends on the memory
+        // address and the hashcode will differ and the bucket will store it in
+        // different index
+        // Identity hashcode and ==
     }
 }
