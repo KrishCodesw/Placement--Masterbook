8310c0c95b79ad91c4cabb13979e4ff3a8993ae9
KrishCodesw <krishjain.w@gmail.com>
2026-04-22 05:49:39 +0530

    2026-04-22 05:49:39

--- javacollectionframeworks/ImmputableMap/ImmutableMapDemo.java
+++ javacollectionframeworks/ImmputableMap/ImmutableMapDemo.java
@@ -3,12 +3,17 @@ import java.util.Map;
 import java.util.Collections;
 
 public class ImmutableMapDemo {
-    Map<String, String> mutableMap = new HashMap<>();
+    public static void main(String[] args) {
+        Map<String, String> mutableMap = new HashMap<>();
+        mutableMap.put("USA", "North America");
 
-    mutableMap.put("USA","North America");
+        Map<String, String> unmodifiableMap = Collections.unmodifiableMap(mutableMap);
+        try {
+            unmodifiableMap.put("Canada", "North America");
+            System.out.println("No exception thrown (unexpected)");
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Exception caught as expected");
+        }
+    }
 
-Map<String, String> unmodifiableMap = Collections.unmodifiableMap(mutableMap);
-
-assertThrows(UnsupportedOperationException.class,() ->unmodifiableMap.put("Canada", "North America"));
-  
 }
