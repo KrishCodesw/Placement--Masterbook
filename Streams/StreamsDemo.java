a6ffa3e0ca100c65db33643d129ca575605099c8
KrishCodesw <krishjain.w@gmail.com>
2026-05-06 12:01:34 +0530

    2026-05-06 12:01:34

--- Streams/StreamsDemo.java
+++ Streams/StreamsDemo.java
@@ -1,14 +1,20 @@
+import java.util.Arrays;
+
 public class StreamsDemo {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 45, 5, 6, 7, 8, 9 };
         int sum = 0;
-        for(int i =0;i<=arr.length-1;i++){
-            if(arr[i]%2==0){
-                sum=sum+arr[i];
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] % 2 == 0) {
+                sum = sum + arr[i];
             }
         }
-        //Above is called imperative programming
-        //Below is called declarative programming   
-        
+        System.out.println(sum);
+        // Above is called imperative programming
+        // Below is called declarative programming
+        int[] arr2 = { 1, 2, 3, 4, 45, 5, 6, 7, 8, 9, 10 };
+        int sum2 = Arrays.stream(arr2).filter(n -> n % 2 == 0).sum();
+        System.out.println(sum2);
+
     }
 }
