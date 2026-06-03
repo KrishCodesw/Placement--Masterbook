e844c65bdfb40e017b6c96dad41b9963ee6ebc2d
KrishCodesw <krishjain.w@gmail.com>
2026-06-03 22:05:39 +0530

    2026-06-03 22:05:39

--- javacollectionframeworks/Iterator/Iteratordemo.java
+++ javacollectionframeworks/Iterator/Iteratordemo.java
@@ -1,3 +1,8 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Iteratordemo {
     public static void main(String[] args) {
         // Create a list of integers
