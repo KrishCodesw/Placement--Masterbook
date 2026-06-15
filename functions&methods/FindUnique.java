8dcb92738bdd493b9a277e5d831e1f0b491d4112
KrishCodesw <krishjain.w@gmail.com>
2026-06-15 21:30:10 +0530

    2026-06-15 21:30:10

--- functions&methods/FindUnique.java
+++ functions&methods/FindUnique.java
@@ -2,7 +2,7 @@ public class FindUnique {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
         int unique = findUnique(arr);
-        System.out.println("The unique element is: " + unique);
+        System.out.println("The unique element is " + unique);
     }
 
     private static int findUnique(int[] arr) {
