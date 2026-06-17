165abac0935f85eb256342517c93ce7ac3fe6a1a
KrishCodesw <krishjain.w@gmail.com>
2026-06-17 17:02:05 +0530

    2026-06-17 17:02:05

--- functions&methods/NoofDigitsinBaseB.java
+++ functions&methods/NoofDigitsinBaseB.java
@@ -2,6 +2,11 @@ public class NoofDigitsinBaseB {
     public static void main(String[] args) {
         int ans = CountofDigits(2, 10);
         System.out.println(ans);
+        int n = 10090776;
+        int b = 10;
+        int ans2 = (int) (Math.log(n) / Math.log(b)) + 1;
+        System.out.println(ans2);
+
     }
 
     private static int CountofDigits(int b, int n) {
