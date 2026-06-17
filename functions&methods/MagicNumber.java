20c24b1233f62b370c8f5b5ad403d2039bf73c99
KrishCodesw <krishjain.w@gmail.com>
2026-06-17 12:27:50 +0530

    2026-06-17 12:27:50

---
+++ functions&methods/MagicNumber.java
@@ -0,0 +1,20 @@
+public class MagicNumber {
+    public static void main(String[] args) {
+
+        int n = 6;
+        int ans = Magic(n);
+        System.out.println(ans);
+
+    }
+
+    private static int Magic(int n) {
+        int magic = 0;
+
+        while (n > 0) {
+            
+        }
+
+        return magic;
+
+    }
+}
