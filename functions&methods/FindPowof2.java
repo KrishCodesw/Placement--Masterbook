ff68ee74f7bdee6a22fa9798b95f0e93c1605bc8
KrishCodesw <krishjain.w@gmail.com>
2026-06-17 19:13:20 +0530

    2026-06-17 19:13:20

--- functions&methods/FindPowof2.java
+++ functions&methods/FindPowof2.java
@@ -1,6 +1,31 @@
+// public class FindPowof2 {
+//     public static void main(String[] args) {
+//         int n = 257;
+//         boolean ans = FindIfPowof2(n);
+//         System.out.println(ans);
+//     }
+
+//     private static boolean FindIfPowof2(int n) {
+//         boolean ans = true;
+
+//         if (n <= 0) {
+//             return false;
+//         }
+//         while (n > 1) {
+//             if ((n & 1) != 0) {
+//                 ans = false;
+//             }
+//             n = n >> 1;
+//         }
+
+//         return ans;
+
+//     }
+// }
+
 public class FindPowof2 {
     public static void main(String[] args) {
-        int n = 257;
+        int n = 128;
         boolean ans = FindIfPowof2(n);
         System.out.println(ans);
     }
