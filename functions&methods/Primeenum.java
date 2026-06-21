4752e1d69c90bf22716983f36218735199952dc2
KrishCodesw <krishjain.w@gmail.com>
2026-06-21 13:35:14 +0530

    2026-06-21 13:35:14

--- functions&methods/Primeenum.java
+++ functions&methods/Primeenum.java
@@ -1,21 +1,25 @@
+
 public class Primeenum {
     public static void main(String[] args) {
         System.out.println(isPrime(7));
         System.out.println(isPrime(17));
-        System.out.println(isPrime(9));
-        System.out.println(isPrime(19));
-        System.out.println(isPrime(25));
     }
 
     private static boolean isPrime(int n) {
-        if (n < 2) {
+        if (n < 2)
             return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        if (n == 2)
+            return true;
+        if (n % 2 == 0)
+            return false;
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
             if (n % i == 0) {
                 return false;
             }
         }
         return true;
+
     }
+
 }
