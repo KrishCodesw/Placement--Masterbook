316b43c92e4e34c7d1cf3c05ad84c30199d600e8
KrishCodesw <krishjain.w@gmail.com>
2026-06-23 14:33:22 +0530

    2026-06-23 14:33:22

--- functions&methods/NewtonRaphson.java
+++ functions&methods/NewtonRaphson.java
@@ -1,3 +1,20 @@
 public class NewtonRaphson {
-        
+    public static void main(String[] args) {
+        System.out.println(sqrt(40));
+    }
+
+    private static double sqrt(double n) {
+        double x = n;
+        double root;
+        while (true) {
+            root = (x + (n / x)) / 2;
+            double error = Math.abs(root - x);
+            // can do it 0.5 as well
+            if (error < 1) {
+                break;
+            }
+            x = root;
+        }
+        return root;
+    }
 }
