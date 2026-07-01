1efd11c36825e52c909b3b541f8e811b19d1a20c
KrishCodesw <krishjain.w@gmail.com>
2026-07-02 03:03:48 +0530

    2026-07-02 03:03:48

--- RecursionL1/Nto1.java
+++ RecursionL1/Nto1.java
@@ -2,6 +2,7 @@ public class Nto1 {
     public static void main(String[] args) {
         int n = 5;
         Nto1fun(n);
+        Nto1funrev(n);
     }
 
     static void Nto1fun(int n) {
@@ -10,6 +11,15 @@ public class Nto1 {
         }
         System.out.println(n);
         Nto1fun(n - 1);
+
+    }
+
+    static void Nto1funrev(int n) {
+        if (n == 0) {
+            return;
+        }
+        Nto1funrev(n - 1);
+        System.out.println(n);
 
     }
 }
