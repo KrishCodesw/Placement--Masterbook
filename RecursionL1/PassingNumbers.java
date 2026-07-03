35569f78bad5f82007c21732a9ce0a270129af64
KrishCodesw <krishjain.w@gmail.com>
2026-07-03 21:05:31 +0530

    2026-07-03 21:05:31

---
+++ RecursionL1/PassingNumbers.java
@@ -0,0 +1,18 @@
+public class PassingNumbers {
+
+    public static void main(String[] args) {
+
+        int n = 1556;
+        fun(n);
+    }
+
+    static void fun(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+
+        fun(--n);
+    }
+
+}
