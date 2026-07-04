62f56239416708d0115bccff72997c79165575ec
KrishCodesw <krishjain.w@gmail.com>
2026-07-04 20:29:32 +0530

    2026-07-04 20:29:32

--- RecursionL1/ReverseNumber.java
+++ RecursionL1/ReverseNumber.java
@@ -3,9 +3,10 @@ public class ReverseNumber {
         if (n == 0) {
             System.out.print(0);
             return;
-        } 
+        }
         System.out.println(n % 10);
 
         fun(n / 10);
     }
 }
+// No need to return the value since the number is already outside the function
