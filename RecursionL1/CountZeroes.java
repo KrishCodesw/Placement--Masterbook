7d0af1bc988109ecb0de5234f98183974fe5cf93
KrishCodesw <krishjain.w@gmail.com>
2026-07-04 23:16:49 +0530

    2026-07-04 23:16:49

--- RecursionL1/CountZeroes.java
+++ RecursionL1/CountZeroes.java
@@ -1,22 +1,45 @@
-public class CountZeroes {
+// public class CountZeroes {
 
-    public static void main(String[] args) {
-        System.out.println(CountZ(1008204, 0));
-    }
+// public static void main(String[] args) {
+// System.out.println(CountZ(1008204, 0));
+// }
 
-    // 300258
-    // 2
-    static int CountZ(int n, int count) {
+// // 300258
+// // 2
+// static int CountZ(int n, int count) {
 
-        if (n == 0) {
-            return count;
-        }
+// if (n == 0) {
+// return count;
+// }
 
-        if (n % 10 == 0) {
-            return CountZ(n / 10, count + 1);
-        }
+// if (n % 10 == 0) {
+// return CountZ(n / 10, count + 1);
+// }
 
-        return CountZ(n / 10, count);
+// return CountZ(n / 10, count);
 
-    }
-}
+// }
+// }
+
+// OUR OWN APPROACH
+
+// public class CountZeroes {
+
+//     public static void main(String[] args) {
+//         System.out.println(CountZ(1008204, 0));
+//     }
+
+//     // 300258
+//     // 2
+//     static int CountZ(int n, int count) {
+//         if (n % 10 == 0) {
+//             count++;
+//         }
+//         if (n < 10 && n % 10 != 0) {
+//             return count;
+//         }
+//         return CountZ(n / 10, count);
+//     }
+// }
+
+// Is this code correct and good ?
