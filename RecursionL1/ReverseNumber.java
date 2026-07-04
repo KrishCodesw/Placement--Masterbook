7f6c74af6b666da87e5422bb937507ca935d258f
KrishCodesw <krishjain.w@gmail.com>
2026-07-04 20:44:06 +0530

    2026-07-04 20:44:05

--- RecursionL1/ReverseNumber.java
+++ RecursionL1/ReverseNumber.java
@@ -9,12 +9,17 @@ public class ReverseNumber {
         fun(n / 10);
     }
 
-    static void revfun(int n) {
-        if (n == 0) {
-            System.out.print(0);
-            return;
+    static int revfun(int n) {
+        if (n < 10) {
+            return n;
         }
         int digits = (int) (Math.log10(n)) + 1; // How to calculate number of digits in an integer super IMPPPPPPPPPP
+        return (n % 10 * (int) (Math.pow(10, digits - 1)) + revfun(n / 10));
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(revfun(1253));
     }
 }
 // No need to return the value since the number is already outside the function
