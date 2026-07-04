ad98fe6af086d8cc73e1a1139f05068bbc0b992c
KrishCodesw <krishjain.w@gmail.com>
2026-07-04 23:15:41 +0530

    2026-07-04 23:15:41

--- RecursionL1/CountZeroes.java
+++ RecursionL1/CountZeroes.java
@@ -7,12 +7,16 @@ public class CountZeroes {
     // 300258
     // 2
     static int CountZ(int n, int count) {
-        if (n % 10 == 0) {
-            count++;
-        }
-        if (n < 10 && n % 10 != 0) {
+
+        if (n == 0) {
             return count;
         }
+
+        if (n % 10 == 0) {
+            return CountZ(n / 10, count);
+        }
+
         return CountZ(n / 10, count);
+
     }
 }
