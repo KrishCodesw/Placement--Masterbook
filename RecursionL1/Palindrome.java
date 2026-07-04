48bec4d196532f05de68295980fec83e32b665f5
KrishCodesw <krishjain.w@gmail.com>
2026-07-04 21:11:51 +0530

    2026-07-04 21:11:50

---
+++ RecursionL1/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(Palin(621544));
+    }
+
+    static boolean Palin(int n) {
+
+        return n == revfun(n);
+
+    }
+
+    static int revfun(int n) {
+        if (n < 10) {
+            return n;
+        }
+        int digits = (int) (Math.log10(n)) + 1; // How to calculate number of digits in an integer super IMPPPPPPPPPP
+        return (n % 10 * (int) (Math.pow(10, digits - 1)) + revfun(n / 10));
+
+    }
+}
