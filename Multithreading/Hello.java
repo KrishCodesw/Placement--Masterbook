c50ab6e0afe43f67bc50e067508db5d977bcc93b
KrishCodesw <krishjain.w@gmail.com>
2026-07-10 20:55:54 +0530

    2026-07-10 20:55:54

---
+++ Multithreading/Hello.java
@@ -0,0 +1,23 @@
+
+public class Hello extends Thread {
+
+    public static void main(String[] args) {
+
+        World w = new World();
+        w.start();
+
+        for (;;) {
+            System.out.println("Hello" + Thread.currentThread().getName());
+        }
+    }
+    // Random order of executions -> class-> extend thread class -> override run
+    // method -> call start method to initiate new thread
+
+    // @Override
+    // public void run() {
+    // for (int i = 0; i < 10000; i++) {
+    // System.out.println("World");
+    // }
+    // }
+    // }
+}
