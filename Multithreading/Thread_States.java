0c67e92a0a6fd42a1ac168d2ef7d8571d39d1a88
KrishCodesw <krishjain.w@gmail.com>
2026-07-11 06:06:52 +0530

    2026-07-11 06:06:52

--- Multithreading/Thread_States.java
+++ Multithreading/Thread_States.java
@@ -3,14 +3,20 @@ public class Thread_States extends Thread {
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getState() + " " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread_States t1 = new Thread_States();
         System.out.println(t1.getState());
         t1.start();
         System.out.println(t1.getState() + " " + t1.getName());
         System.out.println(Thread.currentThread().getState() + " " + Thread.currentThread().getName());
+        Thread.sleep(100);
 
     }
 }
