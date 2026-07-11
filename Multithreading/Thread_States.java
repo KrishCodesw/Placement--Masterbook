c6f3876223ec7be6a05fde4b0f8d1417b392f4a2
KrishCodesw <krishjain.w@gmail.com>
2026-07-11 05:50:48 +0530

    2026-07-11 05:50:48

--- Multithreading/Thread_States.java
+++ Multithreading/Thread_States.java
@@ -1,3 +1,22 @@
-public class Thread_States {
-    
+public class Thread_States extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getState() + " " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        Thread_States t1 = new Thread_States();
+        System.out.println(t1.getState());
+        t1.start();
+        System.out.println(t1.getState() + " " + t1.getName());
+        System.out.println(Thread.currentThread().getState() + " " + Thread.currentThread().getName());
+
+    }
 }
+
+// Output:
+// NEW
+// RUNNABLE Hello
+// RUNNABLE Thread-0
+// RUNNABLE main
