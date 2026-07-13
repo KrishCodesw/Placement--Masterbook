52046d27c437dbb559d82680fafda6b5d9afce8d
KrishCodesw <krishjain.w@gmail.com>
2026-07-14 02:06:46 +0530

    2026-07-14 02:06:46

--- Multithreading/Count.java
+++ Multithreading/Count.java
@@ -1,7 +1,7 @@
 public class Count {
     private int count = 0;
 
-    public void increment() {
+    public synchronized void increment() {
         count++;
     }
 
--- Multithreading/RelatedtoCount.java
+++ Multithreading/RelatedtoCount.java
@@ -4,7 +4,8 @@ public class RelatedtoCount {
 
         ThreadforCount t1 = new ThreadforCount(c);
         ThreadforCount t2 = new ThreadforCount(c);
-        // Sharing of resource - common object
+        // Sharing of resource - common object between multiple threads - to make sure
+        // only one method can access one object at a time
         t1.start();
         t2.start();
         try {
