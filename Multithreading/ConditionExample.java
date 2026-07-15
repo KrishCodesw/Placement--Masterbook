c3f1e1e4a9b8fddde0f2dd4c3a098ef0e5c82c07
KrishCodesw <krishjain.w@gmail.com>
2026-07-15 21:36:43 +0530

    2026-07-15 21:36:43

--- Multithreading/ConditionExample.java
+++ Multithreading/ConditionExample.java
@@ -3,5 +3,38 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ConditionExample {
+    private static Lock lock = new ReentrantLock();
+    private static Condition conndition = lock.newCondition();
+    private static boolean ready = false;
 
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            lock.lock();
+            try {
+                while (!ready) {
+                    System.out.println("Waiting");
+                    conndition.await();
+                }
+                System.out.println("Thread resumed");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            lock.lock();
+            try {
+                while (!ready) {
+                    System.out.println("Waiting");
+                    conndition.await();
+                }
+                System.out.println("Thread resumed");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+    }
 }
