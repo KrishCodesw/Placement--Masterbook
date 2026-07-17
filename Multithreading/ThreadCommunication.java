e1ae54a73b74b9bf81ccffad536eb50dc2d61aff
KrishCodesw <krishjain.w@gmail.com>
2026-07-17 20:36:41 +0530

    2026-07-17 20:36:41

---
+++ Multithreading/ThreadCommunication.java
@@ -0,0 +1,57 @@
+class SharedResource {
+    private int data;
+    private boolean hasData;
+
+    public void produce(int value) {
+
+    }
+
+    public int consume() {
+        return 0;
+    }
+
+}
+
+class Producer implements Runnable {
+    private SharedResource resource;
+
+    public Producer(SharedResource resource) {
+        this.resource = resource;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            resource.produce(i);
+            System.out.println("Produced: " + i);
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    private SharedResource resource;
+
+    public Consumer(SharedResource resource) {
+        this.resource = resource;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            resource.consume();
+            System.out.println("Consumed: " + i);
+        }
+    }
+}
+
+public class ThreadCommunication {
+    public static void main(String[] args) {
+        SharedResource resource = new SharedResource();
+        Thread producerThread = new Thread(new Producer(resource));
+        Thread consumerThread = new Thread(new Consumer(resource));
+
+        producerThread.start();
+        consumerThread.start();
+        
+    }
+}
