adeedbd93d886e2057caa33adc3227e16c8bee9b
KrishCodesw <krishjain.w@gmail.com>
2026-07-18 20:31:39 +0530

    2026-07-18 20:31:38

--- Multithreading/LambdaExpression.java
+++ Multithreading/LambdaExpression.java
@@ -1,12 +1,27 @@
 public class LambdaExpression {
+    // public static void main(String[] args) {
+    // Runnable runnable = new Runnable() {
+    // @Override
+    // public void run() {
+    // System.out.println("hello");
+    // }
+    // };
+    // Thread t1 = new Thread(runnable);
+    // t1.start();
+    // }
     public static void main(String[] args) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("hello");
-            }
-        };
-        Thread t1 = new Thread(runnable);
+        // Runnable runnable = () -> System.out.println("hello");
+        // Thread t1 = new Thread(runnable);
+        // t1.start();
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("Hello");
+        });
+        // Implementation class of runnable
+        // () -> {
+        // System.out.println("Hello");
+        // }
+
         t1.start();
     }
 }
