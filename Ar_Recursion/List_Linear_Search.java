9c0c64520357386b73d8e1a5a81d6479d508b35e
KrishCodesw <krishjain.w@gmail.com>
2026-07-28 22:25:28 +0530

    2026-07-28 22:25:28

--- Ar_Recursion/List_Linear_Search.java
+++ Ar_Recursion/List_Linear_Search.java
@@ -3,21 +3,24 @@ import java.util.ArrayList;
 
 public class List_Linear_Search {
     public static void main(String[] args) {
-        int[] arr = { 5, 3, 8, 1, 2 };
+        int[] arr = { 5, 3, 8, 1, 8 };
 
         int target1 = 8;
+
+        System.out.println(Lsearch(arr, target1, 0));
     }
 
-    public ArrayList<Integer> Lsearch(int[] arr, int target, int index) {
-        ArrayList<Integer> list = new ArrayList<>();
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    public static ArrayList<Integer> Lsearch(int[] arr, int target, int index) {
 
         if (index == arr.length) {
             return list;
         }
         if (arr[index] == target) {
             list.add(arr[index]);
+            return list;
         }
-
-        return list;
+        return Lsearch(arr, target, index + 1);
     }
 }
