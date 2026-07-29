2fab7da16d1188e5cb099c59c13e775d2da48222
KrishCodesw <krishjain.w@gmail.com>
2026-07-29 10:39:54 +0530

    2026-07-29 10:39:54

--- Ar_Recursion/RotatedBinarySearch.java
+++ Ar_Recursion/RotatedBinarySearch.java
@@ -10,4 +10,30 @@ public class RotatedBinarySearch {
     // s=mid+1
     // else
     // e=mid-1
+    public static void main(String[] args) {
+        int[] arr = { 5, 6, 6, 7, 10, 1, 2, 3 };
+        int key = 7;
+
+        System.out.println("Found at index: " + Lsearch(arr, key, 0, arr.length - 1));
+    }
+
+    public static int Lsearch(int[] arr, int key, int start, int end) {
+        if (start > end)
+            return -1;
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == key)
+            return mid;
+        if (arr[start] <= arr[mid]) {
+            if (key >= arr[start] && key <= arr[mid]) {
+                return Lsearch(arr, key, start, mid - 1);
+            } else {
+                return Lsearch(arr, key, mid + 1, end);
+            }
+        } else if (key >= arr[mid] && key <= arr[end]) {
+            return Lsearch(arr, key, mid + 1, end);
+        } else {
+            return Lsearch(arr, key, start, mid + 1);
+        }
+
+    }
 }
