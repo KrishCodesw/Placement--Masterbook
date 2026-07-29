06a35d66f303dbfd581d4944460f1ea1b16fda12
KrishCodesw <krishjain.w@gmail.com>
2026-07-29 23:22:16 +0530

    2026-07-29 23:22:16

---
+++ recursion/CountZeros.java
@@ -0,0 +1,16 @@
+public class CountZeros {
+    public static void main(String[] args) {
+
+    }
+
+    public static int countZeros(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n % 10 == 0) {
+            return 1 + countZeros(n / 10);
+        } else {
+            return countZeros(n / 10);
+        }
+    }
+}
