b9687e9f3a8c475ce1facce1abcdd8844da7a6ab
KrishCodesw <krishjain.w@gmail.com>
2026-07-29 11:29:12 +0530

    2026-07-29 11:29:12

--- Pattern_Recursion/TrianglePrinting.java
+++ Pattern_Recursion/TrianglePrinting.java
@@ -4,7 +4,20 @@ public class TrianglePrinting {
         // ***
         // **
         // *
-        
+
+        // f(4,0)
+        // f(3,1)
+        // f(2,2)
+        // f(1,3)
+        // f(0,4)
+
     }
 
+    // public static void prinTer(int num, int rows, int cols) {
+    // System.out.print("*");
+    // System.out.println("*");
+
+    // prinTer(num, rows + 1, cols - 1);
+
+    // }
 }
