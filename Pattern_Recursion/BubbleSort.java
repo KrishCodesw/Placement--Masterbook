43370bf2b236f1b5ca86d51411716b21bc98b964
KrishCodesw <krishjain.w@gmail.com>
2026-07-30 07:59:58 +0530

    2026-07-30 07:59:58

--- Pattern_Recursion/BubbleSort.java
+++ Pattern_Recursion/BubbleSort.java
@@ -1,18 +1,28 @@
 public class BubbleSort {
     public static void main(String[] args) {
+        int[] arr = { 4, 3, 2, 1 };
+        sortt(arr, arr.length - 1, 0);
 
+        // Print to verify
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 
-    public static void sortt(int[] arr, int row, int col) {
-        if (row == 0) {
+    public static void sortt(int[] arr, int limit, int idx) {
+        if (limit == 0) {
             return;
         }
-        if (col < row) {
-            if (arr[col] > arr[col + 1]) {
-                int temp = arr[col];
-                arr[col] = arr[col + 1];
-                arr[col + 1] = temp;
+
+        if (idx < limit) {
+            if (arr[idx] > arr[idx + 1]) {
+                int temp = arr[idx];
+                arr[idx] = arr[idx + 1];
+                arr[idx + 1] = temp;
             }
+            sortt(arr, limit, idx + 1);
+        } else {
+            sortt(arr, limit - 1, 0);
         }
     }
 }
