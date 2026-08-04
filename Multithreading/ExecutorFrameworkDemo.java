4ce4e488901c222da594eed9a120a0e734a479b7
KrishCodesw <krishjain.w@gmail.com>
2026-08-04 13:41:10 +0530

    2026-08-04 13:41:10

--- Multithreading/ExecutorFrameworkDemo.java
+++ Multithreading/ExecutorFrameworkDemo.java
@@ -2,5 +2,17 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
 public class ExecutorFrameworkDemo {
-    ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    // no of tasks=5
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        for (int i = 1; i < 5; i++) {
+            final int taskNumber = i;
+            executor.execute(() -> {
+                System.out.println("Task:" + taskNumber + "is executed by" + Thread.currentThread().getName());
+            });
+            // 3 tasks will wait in waiting queue
+        }
+    }
+
 }
