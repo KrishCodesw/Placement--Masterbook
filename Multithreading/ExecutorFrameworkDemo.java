df09bc9af8da225327c827dddbef37e01b0f5683
KrishCodesw <krishjain.w@gmail.com>
2026-08-04 13:29:30 +0530

    2026-08-04 13:29:30

---
+++ Multithreading/ExecutorFrameworkDemo.java
@@ -0,0 +1,6 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+public class ExecutorFrameworkDemo {
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+}
