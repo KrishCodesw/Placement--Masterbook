84b28b68b3da10feb00a36a42a4a56c4891c02a9
KrishCodesw <krishjain.w@gmail.com>
2026-08-09 20:31:29 +0530

    2026-08-09 20:31:29

--- Multithreading/CyclicBarrierDemo.java
+++ Multithreading/CyclicBarrierDemo.java
@@ -42,6 +42,50 @@ public class CyclicBarrierDemo {
         }
     }
 
+    class AggregatorThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            String thisThreadName = Thread.currentThread().getName();
+
+            System.out.println(
+                    thisThreadName + ": Computing sum of " + NUM_WORKERS
+                            + " workers, having " + NUM_PARTIAL_RESULTS + " results each.");
+            int sum = 0;
+
+            for (List<Integer> threadResult : partialResults) {
+                System.out.print("Adding ");
+                for (Integer partialResult : threadResult) {
+                    System.out.print(partialResult + " ");
+                    sum += partialResult;
+                }
+                System.out.println();
+            }
+            System.out.println(thisThreadName + ": Final result = " + sum);
+        }
+    }
+
+    public void runSimulation(int numWorkers, int numberOfPartialResults) {
+        NUM_PARTIAL_RESULTS = numberOfPartialResults;
+        NUM_WORKERS = numWorkers;
+
+        cyclicBarrier = new CyclicBarrier(NUM_WORKERS, new AggregatorThread());
+
+        System.out.println("Spawning " + NUM_WORKERS
+                + " worker threads to compute "
+                + NUM_PARTIAL_RESULTS + " partial results each");
+
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            Thread worker = new Thread(new NumberCruncherThread());
+            worker.setName("Thread " + i);
+            worker.start();
+        }
+    }
+
     public static void main(String[] args) {
+
+        CyclicBarrierDemo demo = new CyclicBarrierDemo();
+        demo.runSimulation(5, 3);
     }
 }
