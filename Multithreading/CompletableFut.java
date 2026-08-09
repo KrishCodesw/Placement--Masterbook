4cd2931f731cc9226601f14b0017020aba56590c
KrishCodesw <krishjain.w@gmail.com>
2026-08-09 11:29:33 +0530

    2026-08-09 11:29:33

--- Multithreading/CompletableFut.java
+++ Multithreading/CompletableFut.java
@@ -3,9 +3,10 @@ import java.util.concurrent.CompletableFuture;
 
 public class CompletableFut {
     public static void main(String[] args) {
-        CompletableFuture<Integer> f1 = CompletableFuture.supplyAsync(() -> 10);
+        CompletableFuture<Integer> f1 = CompletableFuture.supplyAsync(() -> 10).thenApply((result) -> result * 3)
+                .thenApply((result) -> result + 2);
         try {
-            System.out.println(f1.get( ));
+            System.out.println(f1.get());
         } catch (Exception e) {
 
         }
