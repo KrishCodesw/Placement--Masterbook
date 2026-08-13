978de6821f4c53cb85d69af3092d13c17cf25f0a
KrishCodesw <krishjain.w@gmail.com>
2026-08-13 09:42:11 +0530

    2026-08-13 09:42:11

--- 23Pointer/Pointer3.java
+++ 23Pointer/Pointer3.java
@@ -1,10 +1,10 @@
-public class Pointer3 {
-    public static void main(String[] args) {
-        int[] nums = { 1, 2, 0, 1, 0, 2 };
-        // __________ z,o_____________ t
-        int o = 0; // current element of loop
-        while (o < t) {
+// public class Pointer3 {
+//     public static void main(String[] args) {
+//         int[] nums = { 1, 2, 0, 1, 0, 2 };
+//         // __________ z,o_____________ t
+//         int o = 0; // current element of loop
+//         while (o < t) {
 
-        }
-    }
-}
+//         }
+//     }
+// }
