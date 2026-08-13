01ec8edcce724602b8ab7fe2d61daf14d28d8556
KrishCodesw <krishjain.w@gmail.com>
2026-08-13 10:13:12 +0530

    2026-08-13 10:13:12

---
+++ RecursionL1/SkipCharacter.java
@@ -0,0 +1,28 @@
+public class SkipCharacter {
+    // pass the ans string in the arg
+    // create the ans variable in the function body
+    // baccad
+    // bccd
+    // we have to skip "a"
+    // initially empty
+
+    public static void main(String[] args) {
+        skipCh("", "baccad");
+    }
+
+    public static void skipCh(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            System.out.println(processed);
+            return;
+        }
+        char ch = unprocessed.charAt(0);
+        if (ch == 'a') {
+            skipCh(processed, unprocessed.substring(1));
+
+        } else {
+            skipCh(processed + ch, unprocessed.substring(1));
+        }
+
+    }
+
+}
